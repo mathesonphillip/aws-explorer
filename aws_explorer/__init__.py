591e38fcc6250822e9d9bb31631a222398d265a6
Phillip Matheson <matheson.phillip@gmail.com>
2023-02-25 11:02:35 +1100

    Add missing imports

--- aws_explorer/__init__.py
+++ aws_explorer/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
-#  noqa: F401
-
 from .account import Account
 from .account import EC2Manager
+from .account import IAMManager
 from .account import S3Manager
+from .account import STSManager
