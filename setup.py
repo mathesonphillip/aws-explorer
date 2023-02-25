3e1cdeb174e57e947d0e6bce242a18c252db7893
Phillip Matheson <matheson.phillip@gmail.com>
2023-02-25 19:01:39 +1100

    Test: :white_check_mark: Update Notebook for git automation
    Ready to start testing the Github Actions workflow.

--- setup.py
+++ setup.py
@@ -7,7 +7,12 @@ setup(
     version="0.1.0",
     description="A Python package to explore AWS resources",
     author="Phillip Matheson",
+    url="https://github.com/mathesonphillip/aws-explorer",
     author_email="matheson.phillip@gmail.com",
     packages=find_packages(),
-    install_requires=["boto3==1.24.28"],
+    install_requires=[
+        "boto3==1.24.28",
+        "moto==4.1.3",
+        "setuptools==65.5.0",
+    ],
 )
